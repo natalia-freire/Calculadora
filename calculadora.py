2ca00326d7ef8d52c51822a6227cef8ba92b50ae
natalia <natalia.freire@sou.ufac.br>
2022-08-10 15:54:33 -0500

    #5 Corrigir a operação de divisão

--- calculadora.py
+++ calculadora.py
@@ -6,8 +6,11 @@ number_2 = int (input('Enter your second number: '))
 
 print(f'{number_1} + {number_2} =', number_1 + number_2)
 
+#Subtraction
 print(f'{number_1} - {number_2} =', number_1 - number_2)
 
+#Multiplication
 print(f'{number_1} * {number_2} =', number_1 * number_2)
 
-print(f'{number_1} + {number_2} =', number_1 + number_2)
+#Division
+print(f'{number_1} / {number_2} =', number_1 / number_2)
